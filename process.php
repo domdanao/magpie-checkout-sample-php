d32bf8b8c1a5efdc802adbd78b641887f2cc76e2
domdanao <dom@danao.co>
2018-04-18 23:12:18 +0800

    Add integrtion guide popup

--- process.php
+++ process.php
@@ -4,9 +4,9 @@ $post = json_encode($_POST, JSON_PRETTY_PRINT);
 $post2 = nl2br($post);
 $post2 = trim(preg_replace('/\s+/', ' ', $post2));
 
-$content = "<h3>Here is what Checkout generated</h3><br>";
-$content .= "<strong>POST variables:</strong><br>magpie_token: " . $_POST['magpie_token'] . "<br>magpie_email: "  . $_POST['magpie_email'] . "<br><br>";
-$content .= "<strong>In JSON:</strong><br><pre>$post2</pre>";
+$content = "<h3>Checkout Data</h3><br>";
+// $content .= "<strong>POST variables:</strong><br>magpie_token: " . $_POST['magpie_token'] . "<br>magpie_email: "  . $_POST['magpie_email'] . "<br><br>";
+$content .= "<strong>In JSON:</strong><br><pre><code class=\"json\">$post2</code></pre>";
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,7 +14,11 @@ $content .= "<strong>In JSON:</strong><br><pre>$post2</pre>";
     <meta charset="UTF-8">
     <title>Process</title>
     <script src="assets/tingle/tingle.js"></script>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/tingle/tingle.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
+    <script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
   </head>
   <body onload="modal.open();">
 
@@ -22,18 +26,7 @@ $content .= "<strong>In JSON:</strong><br><pre>$post2</pre>";
   var modal = new tingle.modal({
       footer: true,
       stickyFooter: true,
-      closeMethods: [],
-      cssClass: ['custom-class-1', 'custom-class-2'],
-      onOpen: function() {
-          console.log('modal open');
-      },
-      onClose: function() {
-          console.log('modal closed');
-      },
-      beforeClose: function() {
-          // here's goes some logic
-          return false; // nothing happens
-      }
+      closeMethods: []
   });
   modal.setContent('<?php echo $content; ?>');
 
